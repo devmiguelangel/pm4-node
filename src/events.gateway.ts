f6479d8ddf62ea6819ca246f89c5b3d0a8ec0c67
Miguel Angel <miguelangeldev@icloud.com>
2023-08-15 16:04:03 -0400

    feat: add cursor tracking module

---
+++ src/events.gateway.ts
@@ -0,0 +1,44 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() server: Server;
+  clients: Socket[] = [];
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.clients.push(client);
+    console.log('Client connected: ', client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.clients = this.clients.filter((c) => c !== client);
+    console.log('Client disconnected: ', client.id);
+
+    this.server.emit('cursorRemoved', client.id);
+  }
+
+  @SubscribeMessage('mouseMove')
+  handleMousePosition(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: object,
+  ): void {
+    // Get the client id
+    const clientId = client.id;
+
+    // Broadcast the mouse position to the clients
+    client.broadcast.emit('mousePosition', { ...data, id: clientId });
+  }
+}
